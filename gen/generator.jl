9406efbd2d93cae591b9f932184618bf121a0a78
JohnnyChen <johnnychen94@hotmail.com>
2022-01-31 00:55:42 +0800

    codegen: generate low-level header wrapper using Clang 0.15.5

---
+++ gen/generator.jl
@@ -0,0 +1,49 @@
+using Clang.JLLEnvs
+using Clang.Generators
+using JpegTurbo_jll
+
+cd(@__DIR__)
+
+include_dir = normpath(JpegTurbo_jll.artifact_dir, "include")
+
+
+function generate_prologue(options)
+    # Macro "#define LIBJPEG_TURBO_VERSION 2.1.0" is not well-defined values
+    # for Clang to parse. Thus we manually parse and generate it as prologue.
+    function get_jpegturbo_version()
+        lines = readlines(joinpath(include_dir, "jconfig.h"))
+        prefix = "#define LIBJPEG_TURBO_VERSION "
+        version_line = filter(lines) do line
+            startswith(line, prefix)
+        end[1]
+        version_str = strip(split(version_line, prefix)[2])
+        return VersionNumber(version_str)
+    end
+
+    outfile = options["general"]["prologue_file_path"]
+    write(outfile, """
+    const LIBJPEG_TURBO_VERSION = v"$(get_jpegturbo_version())"
+    """)
+end
+
+options = load_options(joinpath(@__DIR__, "generator.toml"))
+
+args = get_default_args()
+push!(args, "-I$include_dir")
+
+# include <stdio.h> so that `size_t` and `FILE` are defined
+system_dirs = map(x->x[9:end], filter(x->startswith(x, "-isystem"), args))
+usrinclude_dir = system_dirs[findfirst(endswith("/usr/include"), system_dirs)]
+header_stdio = joinpath(usrinclude_dir, "stdio.h")
+push!(args, "-include$header_stdio")
+
+generate_prologue(options)
+
+# https://github.com/JuliaInterop/Clang.jl/discussions/373
+headers = [
+    joinpath(include_dir, "jpeglib.h"),
+    joinpath(include_dir, "turbojpeg.h")
+]
+
+ctx = create_context(headers, args, options)
+build!(ctx)
