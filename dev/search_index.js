9b9010d79fa6318d2d4dff8470bc309f2c58d8ca
Documenter.jl <documenter@juliadocs.github.io>
2022-01-30 14:40:11 +0000

    build based on 84dedff

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = JpegTurbo","category":"page"},{"location":"#JpegTurbo","page":"Home","title":"JpegTurbo","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for JpegTurbo.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [JpegTurbo]","category":"page"}]
+}
